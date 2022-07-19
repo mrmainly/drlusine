94fcee009b6d4088d3d2ef3edd23d1a967d83278
ernest <ernest_everstov@mail.ru>
2022-07-19 16:28:57 +0900

    added location

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { Outlet, useParams, useLocation } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Outlet, useParams, useLocation, useNavigate } from "react-router-dom";
 
 import { Header, Footer } from "../components";
+import ROUTES from "../routes";
 
 const Layout = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate(ROUTES.HOME);
+    }, [window.location.reload]);
+
     return (
         <div style={{ backgroundColor: "#F7F7F7" }}>
             <Header />
